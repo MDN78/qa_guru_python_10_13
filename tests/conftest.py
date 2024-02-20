d9a1d4e57f009ed7982dcbaa32213aeedbe4eddb
Dimitri Maksimov <dmitry-maksimov@yandex.ru>
2024-02-20 11:32:52 +0300

    add selenoid driver options

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,7 @@ from selene import browser
 from selenium import webdriver
 from utils import attach
 import allure
-
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture(scope="function", autouse=True)
@@ -15,6 +15,22 @@ def driver_configuration():
         browser.config.window_width = 1920
         browser.config.window_height = 1080
         browser.config.base_url = "https://demoqa.com"
+        options = Options()
+        selenoid_capabilities = {
+            "browserName": "chrome",
+            "browserVersion": "100.0",
+            "selenoid:options": {
+                "enableVNC": True,
+                "enableVideo": True
+            }
+        }
+
+        options.capabilities.update(selenoid_capabilities)
+        driver = webdriver.Remote(
+            command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+            options=options)
+
+        browser.config.driver = driver
 
     yield
     with allure.step('Add screenshot'):
