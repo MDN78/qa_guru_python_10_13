4a59886b916f0c358d2ea46b964dac4729a8f13d
Dimitri Maksimov <dmitry-maksimov@yandex.ru>
2024-02-20 10:37:18 +0300

    update conftest.py - driver configuration

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,33 @@
 import pytest
 from selene import browser
 from selenium import webdriver
+from utils import attach
+import allure
+
 
 
 @pytest.fixture(scope="function", autouse=True)
 def driver_configuration():
-    driver_options = webdriver.ChromeOptions()
-    driver_options.page_load_strategy = 'eager'
-    browser.config.driver_options = driver_options
-    browser.config.window_width = 1920
-    browser.config.window_height = 1080
-    browser.config.base_url = "https://demoqa.com"
+    with allure.step('Driver configuration strategy'):
+        driver_options = webdriver.ChromeOptions()
+        driver_options.page_load_strategy = 'eager'
+        browser.config.driver_options = driver_options
+        browser.config.window_width = 1920
+        browser.config.window_height = 1080
+        browser.config.base_url = "https://demoqa.com"
 
     yield
+    with allure.step('Add screenshot'):
+        attach.add_screenshot(browser)
 
-    browser.quit()
+    with allure.step('Add logs'):
+        attach.add_logs(browser)
+
+    with allure.step('Add html'):
+        attach.add_html(browser)
+
+    with allure.step('Add video'):
+        attach.add_video(browser)
+
+    with allure.step('Close driver'):
+        browser.quit()
