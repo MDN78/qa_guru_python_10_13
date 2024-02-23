3e861b4b5e49c42fdcb646efc90942b2fc26ba28
Dimitri Maksimov <dmitry-maksimov@yandex.ru>
2024-02-23 21:44:31 +0300

    add new branch for extended params

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,15 +8,15 @@ from selenium.webdriver.chrome.options import Options
 from dotenv import load_dotenv
 from utils import attach
 
-DEFAULT_BROWSER_VERSION = '100.0'
+DEFAULT_CHROME_VERSION = '100.0'
+DEFAULT_FIREFOX_VERSION = '99'
 
 
 @allure.step('Select browser version')
 def pytest_addoption(parser):
-    parser.addoption(
-        '--browser_version',
-        default='100.0'
-    )
+    parser.addoption('--browser', action='store', default='chrome', help="Choose browser name.")
+    parser.addoption('--browser_version', default='100.0',
+                     help='Choose browser version. For Chrome: 99.0 or 100.0. For Firefox: 97.0 or 98.0.')
 
 @allure.step('Load env')
 @pytest.fixture(scope='session', autouse=True)
@@ -27,11 +27,17 @@ def load_env():
 @pytest.fixture(scope="function", autouse=True)
 def driver_configuration(request):
     with allure.step('Driver configuration strategy'):
-        browser_version = request.config.getoption('--browser_version')
-        browser_version = browser_version if browser_version != '' else DEFAULT_BROWSER_VERSION
-        driver_options = webdriver.ChromeOptions()
-        driver_options.page_load_strategy = 'eager'
-        browser.config.driver_options = driver_options
+        browser_name = request.config.getoption('--browser')
+        if browser_name.lower() == 'chrome':
+            browser_version = request.config.getoption('--browser_version')
+            browser_version = browser_version if browser_version != '' else DEFAULT_CHROME_VERSION
+            driver_options = webdriver.ChromeOptions()
+            driver_options.page_load_strategy = 'eager'
+            browser.config.driver_options = driver_options
+        elif browser_name.lower() == 'firefox':
+            driver_options = webdriver.ChromeOptions()
+            driver_options.page_load_strategy = 'eager'
+            browser.config.driver_options = driver_options
         browser.config.window_width = 1920
         browser.config.window_height = 1080
         browser.config.base_url = "https://demoqa.com"
