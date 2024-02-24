efcc35d408b3f1c964a0fccaa68056c8dabe84b1
Dimitri Maksimov <dmitry-maksimov@yandex.ru>
2024-02-24 10:56:09 +0300

    update config

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,8 +5,6 @@ from selene import browser
 from selenium import webdriver
 from selenium.webdriver import ChromeOptions
 import allure
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver import FirefoxOptions
 from dotenv import load_dotenv
 from utils import attach
@@ -21,6 +19,7 @@ def pytest_addoption(parser):
     parser.addoption('--browser_version', default='100.0',
                      help='Choose browser version. For Chrome: 99.0 or 100.0. For Firefox: 97.0 or 98.0.')
 
+
 @allure.step('Load env')
 @pytest.fixture(scope='session', autouse=True)
 def load_env():
@@ -33,23 +32,15 @@ def driver_configuration(request):
         browser_name = request.config.getoption('--browser_name')
         browser_version = request.config.getoption('--browser_version')
         browser_version = browser_version if browser_version != '' else DEFAULT_CHROME_VERSION
-        # with allure.step('Select Driver - type and version'):
-        if browser_name.lower() == 'chrome':
-            # browser_version = request.config.getoption('--browser_version')
-            # browser_version = browser_version if browser_version != '' else DEFAULT_CHROME_VERSION
-            driver_options = ChromeOptions()
-            driver_options.page_load_strategy = 'eager'
-            browser.config.driver_options = driver_options
-        elif browser_name.lower() == 'firefox':
-            # browser_version = request.config.getoption('--browser_version')
-            # browser_version = browser_version if browser_version != '' else DEFAULT_FIREFOX_VERSION
-            # driver_options = Options()
-            driver_options = FirefoxOptions()
-            driver_options.page_load_strategy = 'eager'
-            browser.config.driver_options = driver_options
-
-        # browser_version = request.config.getoption('--browser_version')
-        # browser_version = browser_version if browser_version != '' else DEFAULT_CHROME_VERSION
+        with allure.step('Select Driver loading strategy'):
+            if browser_name.lower() == 'chrome':
+                driver_options = ChromeOptions()
+                driver_options.page_load_strategy = 'eager'
+                browser.config.driver_options = driver_options
+            elif browser_name.lower() == 'firefox':
+                driver_options = FirefoxOptions()
+                driver_options.page_load_strategy = 'eager'
+                browser.config.driver_options = driver_options
 
         browser.config.window_width = 1920
         browser.config.window_height = 1080
